e2f032059020d296420aba7e8a29126f39c08624
Rooney <rooney.kamdem@student.vinci.be>
2024-09-23 21:06:00 +0200

    New : exo1.1

--- app.ts
+++ app.ts
@@ -1,14 +1,24 @@
-import express from "express";
+import express, { ErrorRequestHandler } from "express";
 
-import usersRouter from "./routes/users";
-import pizzaRouter from "./routes/pizzas";
+
+import myMoviesRouter from "./routes/films";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/users", usersRouter);
-app.use("/pizzas", pizzaRouter);
 
+
+app.use("/films", myMoviesRouter);
+
+
+
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+    console.error(err.stack);
+    return res.status(500).send("Something broke!");
+  };
+  
+  app.use(errorHandler);
+  
 export default app;
